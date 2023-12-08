23300448da43af19cac376e74e2ce6ea319b1499
Anburaj S <anburaj2203@gmail.com>
2023-12-08 17:30:22 +0530

    symbols and emojis

---
+++ symbol.js
@@ -0,0 +1,8 @@
+//symbol
+console.log(Number(2) === Number(2)); //true
+console.log(Number(2) === 2); //true
+console.log(Number(2)); //2
+console.log(Boolean(false) === Boolean(false)); //true
+console.log(Boolean(false)); //false
+console.log(Symbol("foo") === Symbol("foo")); //false
+console.log(Symbol("foo")); //Symbol(foo)
