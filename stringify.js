1d94377ee9996a79f86ede6b7f06ea53bdf9fc87
Anburaj S <anburaj2203@gmail.com>
2023-12-08 16:57:16 +0530

    function default value

--- stringify.js
+++ stringify.js
@@ -5,4 +5,16 @@ const settings = {
 };
 
 const data = JSON.stringify(settings, ['level', 'health']);
-console.log(data);
+// console.log(data);
+
+const value = { number: 10 };
+
+const multiply = (x = { ...value }) => { // 20 20 20 40
+    // const multiply = (x = value) => { // 20 40 80 160
+  console.log((x.number *= 2));
+};
+
+multiply();
+multiply();
+multiply(value);
+multiply(value);
