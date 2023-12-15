1e5cc6d74f85b31bbe0d566f188a9de5705008a8
Anburaj S <anburaj2203@gmail.com>
2023-12-15 13:06:22 +0530

    rest and array

--- object.js
+++ object.js
@@ -6,4 +6,12 @@ function sayHi(name='Raja') {
     return `Hi there, ${name}`;
   }
   
-  console.log(sayHi());
+  // console.log(sayHi());
+  const person = {
+    name: 'Lydia',
+    age: 21,
+  };
+// console.log(Object.entries(person))  
+for (const [x, y] of Object.entries(person)) {
+  console.log(x, y);
+}
