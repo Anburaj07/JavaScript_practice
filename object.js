d7deb26d9da76c575a1964e52e05089149d363e2
Anburaj S <anburaj2203@gmail.com>
2023-12-18 11:50:23 +0530

    convert keys of obj to UPPERCASE

--- object.js
+++ object.js
@@ -7,22 +7,56 @@ function sayHi(name='Raja') {
   }
   
   // console.log(sayHi());
-  const person = {
-    name: 'Lydia',
-    age: 21,
-  };
-console.log(Object.entries(person))  
-for (const [x, y] of Object.entries(person)) {
-  console.log(x, y);
+  // const person = {
+  //   name: 'Lydia',
+  //   age: 21,
+  // };
+// console.log(Object.entries(person))  
+// for (const [x, y] of Object.entries(person)) {
+//   console.log(x, y);
+// }
+
+// let obj={
+//   name:'a',
+//   age:251,
+//  address:{
+//   doorno:3,
+//   city:"mdu"
+//  }
+// }
+// let st=[]
+// for(key in obj){
+//   st.push(key)
+// }
+// console.log(st)
+
+const person = {
+  name: "blessmi",
+  age: 22,
+  address: {
+    streetname: "ghdfychjdg",
+    mobileno: 123456789
+  },
+  fathername: "Kumaradhas"
+};
+
+// Function to recursively convert keys to uppercase
+function convertKeysToUppercase(obj) {
+  const newObj = {};
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      const newKey = key.toUpperCase();
+      newObj[newKey] =
+        typeof obj[key] === "object" && obj[key] !== null
+          ? convertKeysToUppercase(obj[key])
+          : obj[key];
+    }
+  }
+  return newObj;
 }
 
-let obj={
-  a:'a',
-  b:'b',
-  c:'c'
-}
+// Convert keys to uppercase
+const personUppercase = convertKeysToUppercase(person);
 
-for(key in obj){
-  obj[key]=obj[key].toUpperCase()
-}
-console.log(obj)
+// Output the result
+console.log(personUppercase);
