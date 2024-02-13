2a55b559ba1080918c84bb478fbf8b40a4064d2d
Anburaj S <anburaj2203@gmail.com>
2024-02-13 13:30:40 +0530

    promise timeout

--- promise.js
+++ promise.js
@@ -1,4 +1,22 @@
-let myPromise=Promise.resolve(5);
-myPromise.then((res=>{
-    console.log(res)
-}))
+// let myPromise=Promise.resolve(5);
+// myPromise.then((res=>{
+//     console.log(res)
+// }))
+
+const myPromise = Promise.resolve(Promise.resolve('Promise'));
+
+function funcOne() {
+  setTimeout(() => console.log('Timeout 1!'), 0);
+  myPromise.then(res => res).then(res => console.log(`${res} 1!`));
+  console.log('Last line 1!');
+}
+
+async function funcTwo() {
+  const res = await myPromise;
+  console.log(`${res} 2!`)
+  setTimeout(() => console.log('Timeout 2!'), 0);
+  console.log('Last line 2!');
+}
+
+funcOne();
+funcTwo();
